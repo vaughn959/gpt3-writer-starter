0df49507c66d0e987c16fa1564760436249f467e
vaughn959 <122217561+vaughn959@users.noreply.github.com>
2023-03-10 15:21:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = () => {
             <h1>Simplified Painting</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>ask your painting questions</h2>
           </div>
         </div>
       </div>
